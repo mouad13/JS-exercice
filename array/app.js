ae90d2788776e9b12baa0038d71ed9a883c27d74
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-27 10:44:58 +0200

    Exercice 2

--- array/app.js
+++ array/app.js
@@ -1,6 +1,8 @@
 var mois = ['Janvier','fevrier','mars','avril', 'mai', 'juin', 'juiellet', 'aout', 'septembre', 
 'octobre', 'novembre', 'decembre'];
 
-
+// Exercice 1
 	console.log(mois);
 	
+// Exercice 2
+	console.log(mois[2]);	
