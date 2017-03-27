a228fff795dc2d3c97f22bf95a4d4f533a1a43ac
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-27 10:31:36 +0200

    exo1

--- array/app.js
+++ array/app.js
@@ -1,1 +1,6 @@
-var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var mois = ['Janvier','fevrier','mars','avril', 'mai', 'juin', 'juiellet', 'aout', 'septembre', 
+'octobre', 'novembre', 'decembre'];
+
+
+	console.log(mois);
+	
