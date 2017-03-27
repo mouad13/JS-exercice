ec92fea1928b7a3b921ce10521dd1f5754e492e4
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-27 11:42:50 +0200

    Exercice 9

--- array/exoapp.js
+++ array/exoapp.js
@@ -1,4 +1,9 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
+// Exercice 8
 	array.push("courgette");
 	console.log(array);
+
+// Exercice 9
+	array.splice(4, 0, "Citron")
+	console.log(array);	
